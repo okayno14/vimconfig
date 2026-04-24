682a8c146fd6059332692644a05abf655707c881
aleksandr alekseev <okayno14@gmail.com>
2026-04-24 10:51:49 +0700

    Обновление пакетов

--- .vim/projects_plugin/ssw.shell.nix
+++ .vim/projects_plugin/ssw.shell.nix
@@ -1,14 +1,16 @@
 let
-    pkgs = import <nixpkgs> {};
+    pkgs_otp27 = import (builtins.fetchTarball {
+          url = "https://github.com/NixOS/nixpkgs/archive/e6f23dc08d3624daab7094b701aa3954923c6bbb.tar.gz";
+    }) {};
 in
 
-pkgs.mkShell {
-  packages = with pkgs; [
-    elixir
-    erlang
-    rebar3
+pkgs_otp27.mkShell {
+  packages = with pkgs_otp27; [
+    gcc
+    beam27Packages.erlang
+    beam27Packages.rebar3
     erlang-language-platform
-    erlfmt
+    beam27Packages.erlfmt
   ];
 }
 
