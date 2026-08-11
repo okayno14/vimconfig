112f35f515d8ebc7b2fd0219ca8dc92c5c65ec9d
Aleksandr Alekseev <aleksandr.alekseev@eltex.loc>
2026-08-11 12:25:25 +0700

    fix otp_24

--- .vim/projects_plugin/otp_24.nix
+++ .vim/projects_plugin/otp_24.nix
@@ -1,15 +1,13 @@
 let
-    pkgs_otp27 = import (builtins.fetchTarball {
-          url = "https://github.com/NixOS/nixpkgs/archive/e6f23dc08d3624daab7094b701aa3954923c6bbb.tar.gz";
+    pkgs_otp24 = import (builtins.fetchTarball {
+          url = "https://github.com/NixOS/nixpkgs/archive/05ae8b52071ff158a4d3c7036e13a2e932b2549b.tar.gz";
     }) {};
 in
 
-pkgs_otp27.mkShell {
-  packages = with pkgs_otp27; [
-    gcc
-    beam27Packages.erlang
-    beam27Packages.rebar3
-    erlang-language-platform
-    beam27Packages.erlfmt
+pkgs_otp24.mkShell {
+  packages = with pkgs_otp24; [
+    erlang
+    elixir
+    rebar3
   ];
 }
