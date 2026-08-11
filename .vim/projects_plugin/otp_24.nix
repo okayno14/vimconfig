980f2a4f1d84b4022dfe136e71e0d53ca9350caf
Aleksandr Alekseev <aleksandr.alekseev@eltex.loc>
2026-08-11 11:43:38 +0700

    Добавил nix-shell для otp24

---
+++ .vim/projects_plugin/otp_24.nix
@@ -0,0 +1,15 @@
+let
+    pkgs_otp27 = import (builtins.fetchTarball {
+          url = "https://github.com/NixOS/nixpkgs/archive/e6f23dc08d3624daab7094b701aa3954923c6bbb.tar.gz";
+    }) {};
+in
+
+pkgs_otp27.mkShell {
+  packages = with pkgs_otp27; [
+    gcc
+    beam27Packages.erlang
+    beam27Packages.rebar3
+    erlang-language-platform
+    beam27Packages.erlfmt
+  ];
+}
